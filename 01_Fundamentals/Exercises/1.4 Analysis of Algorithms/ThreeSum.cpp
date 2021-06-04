256c546f74936303e8a0c48a424c4a717c1e3f82
Kaiwen Wang <herrwang.kevin@gmail.com>
2021-06-04 23:59:10 +0200

    Add ThreeSum

---
+++ 01_Fundamentals/Exercises/1.4 Analysis of Algorithms/ThreeSum.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+int threeSum(std::vector<int> vec) {
+    clock_t start, end;
+    start = clock();
+    int count = 0, n = vec.size();
+    for (int i = 0; i != n; ++i) {
+        for (int j = i + 1; j != n; ++j) {
+            for (int k = j + 1; k != n; ++k) {
+                if (vec[i] + vec[j] + vec[k] == 0)
+                    ++count;
+            }
+        }
+    }
+    end = clock();
+    std::cout << "elapsed time = " << static_cast<double>(end - start) / CLOCKS_PER_SEC
+              << "s" << std::endl;
+    return count;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        std::cout << "Please check your input!" << std::endl;
+        return -1;
+    }
+    std::ifstream in(argv[1]);
+    std::vector<int> vec;
+    int num;
+    while (in >> num)
+        vec.push_back(num);
+    std::cout << threeSum(vec) << std::endl;
+    return 0;
+}
